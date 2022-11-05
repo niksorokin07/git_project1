b196a9bee14e7d470fc1c7b5f7c964e81a058fd3
niksorokin07 <117514209+niksorokin07@users.noreply.github.com>
2022-11-05 14:41:10 +0300

    first.py

--- first.py
+++ first.py
@@ -1,19 +1,6 @@
-# Импорт библиотеки
-import sqlite3
+def main():
+    print('My first git-repo')
 
-# Подключение к БД
-con = sqlite3.connect(input())
 
-# Создание курсора
-cur = con.cursor()
-
-# Выполнение запроса и получение всех результатов
-result = cur.execute("""SELECT * FROM Films
-    WHERE duration >= 60 AND
-     genre=(SELECT id FROM genres WHERE title = 'комедия') """).fetchall()
-
-# Вывод результатов на экран
-for elem in result:
-    print(elem[1])
-
-con.close()
+if __name__ == '__main__':
+    main()
